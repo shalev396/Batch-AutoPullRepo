e08efd411eaa79eae1747b9f2172945add56b986
shalev396 <shalev396@gmail.com>
2024-09-11 20:06:03 +0300

    added the batch file

---
+++ UpdateRepo.bat
@@ -0,0 +1,17 @@
+REM:my files
+C: && cd C:\Iitc && git.exe pull
+C: && cd C:\Projects\ApiTesting && git.exe pull
+C: && cd C:\Projects\PasswordProtector && git.exe pull
+C: && cd C:\Projects\Android-project-Fleemarket && git.exe pull
+C: && cd C:\Projects\Android-project-komezz && git.exe pull
+C: && cd C:\Projects\Java-project-KomezzFileMonitor && git.exe pull
+
+REM:techer files
+REM:Omer files
+C: && cd C:\Iitc Techers Files\Omer\js-basics-inclass && git.exe pull
+c: && cd C:\Iitc Techers Files\Omer\IITC-Full-Stack-3-in-class- && git.exe pull
+REM:Israel files
+C: && cd C:\Iitc Techers Files\Israel\SQL_NODE && git.exe pull
+REM:Liron files
+C: && cd C:\Iitc Techers Files\Liron\TA-at-IITC && git.exe pull
+pause

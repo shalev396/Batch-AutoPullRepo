a897027feb146d9ddc12bc4145e0889920d55e67
shalev396 <shalev396@gmail.com>
2024-12-25 11:05:21 +0200

    updated repos

--- UpdateRepo.bat
+++ UpdateRepo.bat
@@ -1,14 +1,27 @@
-REM:my files
+REM:iitc
 C: && cd C:\Iitc && git.exe pull
-C: && cd C:\Projects\ApiTesting && git.exe pull
-C: && cd C:\Projects\PasswordProtector && git.exe pull
+REM:my projects
 C: && cd C:\Projects\Android-project-Fleemarket && git.exe pull
 C: && cd C:\Projects\Android-project-komezz && git.exe pull
-C: && cd C:\Projects\Java-project-KomezzFileMonitor && git.exe pull
+C: && cd C:\Projects\ApiTesting && git.exe pull
 C: && cd C:\Projects\Batch-AutoPullRepo && git.exe pull
+C: && cd C:\Projects\BooksAPI && git.exe pull
+C: && cd C:\Projects\Business-Finder && git.exe pull
+C: && cd C:\Projects\CallOfDutyProject && git.exe pull
+C: && cd C:\Projects\First-Full-Stack && git.exe pull
+C: && cd C:\Projects\Fit-by-click && git.exe pull
 C: && cd C:\Projects\Frontend-Mentor-projects && git.exe pull
+
+C: && cd C:\Projects\Java-project-KomezzFileMonitor && git.exe pull
+C: && cd C:\Projects\lone-soldier && git.exe pull
+C: && cd C:\Projects\Mock-Social-Network && git.exe pull
+C: && cd C:\Projects\PasswordProtector && git.exe pull
 C: && cd C:\Projects\red-Alerts && git.exe pull
+C: && cd C:\Projects\shalev396 && git.exe pull
 C: && cd C:\Projects\TheMovieDataBase && git.exe pull
+
+
+C: && cd C:\Projects\ && git.exe pull
 REM:techer files
 REM:Omer files
 C: && cd C:\Iitc Techers Files\Omer\js-basics-inclass && git.exe pull
@@ -24,5 +37,8 @@ C: && cd C:\Projects\HomeWork-Small-Projects\basic-node-js-shalev396  && git.exe
 C: && cd C:\Projects\HomeWork-Small-Projects\big-api-project-shalev396 && git.exe pull
 C: && cd C:\Projects\HomeWork-Small-Projects\frontend-mentor-faq-accordion-shalev396 && git.exe pull
 C: && cd C:\Projects\HomeWork-Small-Projects\frontend-mentor-interactive-rating-component-shalev396 && git.exe pull
+C: && cd C:\Projects\HomeWork-Small-Projects\pokemons-react-project-shalev396 && git.exe pull
+C: && cd C:\Projects\HomeWork-Small-Projects\social-network-fs-project-shalev-sasha-and-ron-instagram-mock && git.exe pull
 C: && cd C:\Projects\HomeWork-Small-Projects\sukkot-assignment-shalev396 && git.exe pull
+
 pause
